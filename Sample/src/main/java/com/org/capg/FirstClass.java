4953851ddbf5930265bc5f7df08e1d706f95a10f
ararunac <ararunac@DIN78000418.corp.capgemini.com>
2019-07-04 12:04:28 +0530

    this is my first git huib

---
+++ Sample/src/main/java/com/org/capg/FirstClass.java
@@ -0,0 +1,8 @@
+package com.org.capg;
+
+public class FirstClass {
+	public void display() {
+		System.out.println("print this");
+	}
+
+}
---
+++ Sample/src/main/java/com/org/capg/MainClass.java
@@ -0,0 +1,9 @@
+package com.org.capg;
+
+public class MainClass {
+public void main(String args[]) {
+	FirstClass f=new FirstClass();
+	f.display();
+	
+}
+}
